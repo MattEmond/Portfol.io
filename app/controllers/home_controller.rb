0de0293a94ec802b565541f7328205ae49d30c1b
Matthew Emond <matthewemond@Matthews-MacBook-Pro.local>
2017-12-21 15:54:08 -0500

    Set up JSON

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,12 @@ class HomeController < ApplicationController
       if params[:id]
         begin
           @stock = StockQuote::Stock.quote(params[:id])
+          @stock_json = StockQuote::Stock.json_quote(params[:id])
         rescue StandardError
           @error = "That stock symbol is not valid"
         end
 
-      end 
+      end
     end
   end
 
