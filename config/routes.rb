43be0ccc0958544062979eb5da3dc1af37f510d2
Matthew Emond <matthewemond@Matthews-MacBook-Pro.local>
2017-12-20 21:46:30 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   #get 'home/index'
   root 'home#index'
   get 'home/about'
+  get 'conversations/index'
+  resources :users, only: [:index]
+  resources :personal_messages, only: [:new, :create]
 
   post "/" => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
