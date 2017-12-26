753de8b768bdd5062e8fbbaea2387f9dc195b51c
montygoldy <montygoldy2006@gmail.com>
2017-12-26 17:45:56 +0000

    Added remove and edit to /stocks page as well as made the whole row clickable

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'home/about'
 
   get 'stocks/historical_chart/:stock', :to => 'stocks#historical_chart'
+  get 'stocks/stock_news/:stock', :to => 'stocks#stock_news'
 
   post "/" => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
